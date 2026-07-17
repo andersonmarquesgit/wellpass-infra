7b865b28cce9e08512d4c4552f59b150703a8047
Anderson Marques <andersonmarques.ci@gmail.com>
2026-07-17 15:02:36 -0300

    fix(tofu): use valid multi-line output blocks

--- infrastructure/terraform/modules/workload-cluster/outputs.tf
+++ infrastructure/terraform/modules/workload-cluster/outputs.tf
@@ -1,6 +1,25 @@
-output "cluster_name" { value = module.eks.cluster_name }
-output "external_secrets_role_arn" { value = aws_iam_role.external_secrets.arn }
-output "cluster_endpoint" { value = module.eks.cluster_endpoint, sensitive = true }
-output "vpc_id" { value = module.vpc.vpc_id }
-output "database_endpoint" { value = aws_rds_cluster.postgres.endpoint, sensitive = true }
-output "database_master_secret_arn" { value = aws_rds_cluster.postgres.master_user_secret[0].secret_arn }
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "external_secrets_role_arn" {
+  value = aws_iam_role.external_secrets.arn
+}
+
+output "cluster_endpoint" {
+  value     = module.eks.cluster_endpoint
+  sensitive = true
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "database_endpoint" {
+  value     = aws_rds_cluster.postgres.endpoint
+  sensitive = true
+}
+
+output "database_master_secret_arn" {
+  value = aws_rds_cluster.postgres.master_user_secret[0].secret_arn
+}
